17d66a6ff7895018e95212e5ea9962ce490b6e38
Mauricio Lima <github@mauriciolima.eti.br>
2019-12-17 09:26:40 +0000

    Introducing dicotomy algorithm that runs with few iterations, but the complexity of function make that become the slowest

--- main.c
+++ main.c
@@ -40,6 +40,52 @@ int algebric(float salaryCarlos, float salaryJoao, float rateCarlos, float rateJ
 }
 
 
+float equation(float x, float salaryCarlos, float salaryJoao, float rateCarlos, float rateJoao)
+{
+  return (salaryCarlos/salaryJoao) * pow(rateCarlos/rateJoao, x);
+}
+
+
+int dicotomy(float salaryCarlos, float salaryJoao, float rateCarlos, float rateJoao, int* iterations)
+{
+  float left;
+  float right;
+  float middle;
+
+  *iterations = 0;
+  left = 1;
+  while (1)
+  {
+    if ( (equation(left, salaryCarlos, salaryJoao, rateCarlos, rateJoao) > 1) && (equation(left * 2, salaryCarlos, salaryJoao, rateCarlos, rateJoao) < 1) )
+      break;
+
+    left *= 2;
+    (*iterations)++;
+  }
+  right = left * 2;
+
+  while (1)
+  {
+    middle = (left + right) / 2;
+    if ( (int)(equation(middle, salaryCarlos, salaryJoao, rateCarlos, rateJoao) * 1000) == 1000 )
+    {
+      return round(middle + 1);
+    }
+
+    if ( (equation(right, salaryCarlos, salaryJoao, rateCarlos, rateJoao) < 1) && (equation(middle, salaryCarlos, salaryJoao, rateCarlos, rateJoao) < 1) )
+    {
+      right = middle; 
+    }
+    else
+    {
+      left = middle;
+    }
+
+    (*iterations)++;
+  }
+}
+
+
 void runner(runner_t* function, char* name, int count, float salaryCarlos, float salaryJoao, float rateCarlos, float rateJoao)
 {
   clock_t start;
@@ -72,6 +118,7 @@ int main(void)
 
   runner(loop,     "loop",     3000, salaryCarlos, salaryCarlos / 3, rateCarlos, rateJoao);
   runner(algebric, "algebric", 3000, salaryCarlos, salaryCarlos / 3, rateCarlos, rateJoao);
+  runner(dicotomy, "dicotomy", 3000, salaryCarlos, salaryCarlos / 3, rateCarlos, rateJoao);
 
   return 0;
 }
