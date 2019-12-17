206e248cf63d54309763d45fb17ca3379d5520bf
Mauricio Lima <github@mauriciolima.eti.br>
2019-12-17 07:41:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main(void) 
+{
+  float taxCarlos = 1.02;
+  float taxJoao   = 1.05;
+  float salaryCarlos;
+
+  printf("Qual o salário de Carlos? ");
+  scanf("%f", &salaryCarlos);
+
+  return 0;
+}
