fb5c8e1927e7ca2a70038017c58ed729c0152869
Mauricio Lima <github@mauriciolima.eti.br>
2019-12-17 08:01:26 +0000

    Solution with loop

--- main.c
+++ main.c
@@ -2,14 +2,41 @@
 
 #include <stdio.h>
 
+
+void loop(float salaryCarlos, float rateCarlos, float rateJoao)
+{
+  float balanceCarlos;
+  float balanceJoao;
+  int   months;
+
+  months = 0;
+  balanceCarlos = salaryCarlos;
+  balanceJoao   = salaryCarlos / 3;
+  while (1)
+  {
+    balanceCarlos *= rateCarlos;
+    balanceJoao   *= rateJoao;
+
+    if (balanceCarlos < balanceJoao)
+      break;
+
+    months++;
+  }
+
+  printf("\nSaldo atingido em %d meses\n\n", months+1);
+}
+
+
 int main(void) 
 {
-  float taxCarlos = 1.02;
-  float taxJoao   = 1.05;
+  float rateCarlos = 1.02;
+  float rateJoao   = 1.05;
   float salaryCarlos;
 
   printf("Qual o salário de Carlos? ");
   scanf("%f", &salaryCarlos);
+
+  loop(salaryCarlos, rateCarlos, rateJoao);
 
   return 0;
 }
